6e923a8de091edc9ce5e7c8fa7aaa41d90acc109
robertocraig <robertodermatocdia@gmail.com>
2024-08-25 17:20:37 +0000

    Modificando train.py

--- train.py
+++ train.py
@@ -27,27 +27,6 @@ def train_step(model, dataloader, loss_fn, optimizer, device):
     train_loss /= len(dataloader)
     return train_loss
 
-# Função para o loop de validação
-def test_step(model, dataloader, loss_fn, device):
-    model.eval()  # Configura o modelo para modo de avaliação    
-    test_loss, correct = 0, 0
-
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-
-            # Forward pass
-            y_pred = model(X)
-            test_loss += loss_fn(y_pred, y).item()
-            
-            # Calcula a acurácia            
-            _, preds = torch.max(y_pred, 1)
-            correct += (preds == y).sum().item()
-
-    test_loss /= len(dataloader)
-    accuracy = correct / len(dataloader.dataset)
-    return test_loss, accuracy
-
 # Função para imprimir o tempo de treinamento
 def print_train_time(start: float, end: float, device: torch.device = None):
     total_time = end - start
